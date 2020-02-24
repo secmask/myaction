f9b80e0b336d4a97f50098e60f14176338442c67
GiangTran <secmask@gmail.com>
2020-02-24 18:16:00 +0700

    add test pg

--- cmd/myaction/main_test.go
+++ cmd/myaction/main_test.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"log"
 	"testing"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 func TestAA(t *testing.T) {
 	if Add(1,2)!=3{
 		t.Fatal("not equal")
 	}
+	connStr := "user=postgres dbname=postgres password=secret sslmode=disable"
+	db, err := sqlx.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var res string
+	err = db.Get(&res,"SELECT version()")
+	if err!=nil{
+		t.Fatal(err)
+	}
+	t.Log(res)
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module myaction
 
 go 1.13
+
+require (
+	github.com/jmoiron/sqlx v1.2.0 // indirect
+	github.com/lib/pq v1.3.0 // indirect
+)
