b4a32697b5b43b4678acfb9271f3c456dfe9f7ec
GiangTran <secmask@gmail.com>
2024-08-10 09:45:59 +0700

    more change

--- cmd/myaction/main_test.go
+++ cmd/myaction/main_test.go
@@ -11,6 +11,7 @@ import (
 func TestAB(t *testing.T) {
 	resp, err := http.Get("https://ifconfig.me")
 	require.NoError(t, err)
+
 	data, err := io.ReadAll(resp.Body)
 	require.NoError(t, err)
 	t.Log(string(data))
